1fdbaa5821484801d3e2582d451f770101cb0153
Gusts Linkevics <gusts@gst.lv>
2021-06-10 19:39:47 +0300

    Button images added

--- TicTacToeWeb/Pages/Index.cshtml.cs
+++ TicTacToeWeb/Pages/Index.cshtml.cs
@@ -5,12 +5,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicTacToeData;
 
 namespace TicTacToeWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Game TicTacToeGame { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +23,29 @@ namespace TicTacToeWeb.Pages
 
         public void OnGet()
         {
+            TicTacToeGame = new Game();
+        }
 
+        public string GetFieldImage(int row, int col)
+        {
+            if(TicTacToeGame != null)
+            {
+                return string.Empty;
+            }
+
+            var field = TicTacToeGame.GetFieldValue(row, col);
+
+            switch (field)
+            {
+                case FieldTypeEnum.Empty:
+                    return string.Empty;
+                case FieldTypeEnum.X:
+                    return "<img src=\"/img/x.png\"/>";
+                case FieldTypeEnum.O:
+                    return "<img src=\"/img/o.png\"/>";
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
